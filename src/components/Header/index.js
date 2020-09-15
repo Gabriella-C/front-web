8371fb5c9313bbf12fe18abf6841bd8f5ef73ec1
Gabriella-C <gabriella.cristinec@gmail.com>
2020-09-14 21:11:20 -0300

    ajuste de texto no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,17 +3,19 @@ import React from 'react';
 import { Container } from './styles';
 
 function Header() {
-  return <Container>
-    <div>
-      <ul>
-        <li>Home</li>
-        <li>Entregador</li>
-        <li>Restaurante</li>
-      </ul>
+  return (
+    <Container>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Entregador</li>
+          <li>Petshop</li>
+        </ul>
 
-      <button>Entrar</button>
-    </div>
-  </Container>;
+        <button>Entrar</button>
+      </div>
+    </Container>
+  );
 }
 
 export default Header;
